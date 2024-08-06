fd97fc2936b27ca43bfe44d76ac55f6ed0c2a87d
hwait123 <hwait12@ufl.edu>
2024-08-05 21:04:47 -0400

    Completed menu update.

--- Weather_Wizard/src/main.cpp
+++ Weather_Wizard/src/main.cpp
@@ -8,94 +8,99 @@
 string FindCity (string input);
 bool IsValidCity(string city);
 bool IsValidDate(City city, string date);
-bool IsValidRange(string arrivalDate, string departureDate);
-void LoadAllCities();
+bool IsValidRange(string startDate, string endDate);
 
 int main()
 {
     string cityNum;
     string cityName;
-    string arrivalInput;
-    string departureInput;
-
-    City ReadAllFiles("Gainesville");
+    string startDate;
+    string endDate;
 
     cout << "Welcome to the Weather Wizard!" << endl;
-    cout << "If you are planning a trip to the beautiful state of Florida, Weather Wizard is here to help!" << endl << endl;
+    cout << "If you are researching weather patterns in the beautiful state of Florida, Weather Wizard is here to help!" << endl << endl;
 
     //ASCII logo here, if possible
 
-    cout << "Weather Wizard is programmed to provide weather information for the following cities:" << endl;
-    cout << "1. Gainesville" << endl;
-    cout << "2. Jacksonville" << endl;
-    cout << "3. Miami" << endl;
-    cout << "4. Orlando" << endl;
-    cout << "5. Tampa" << endl; 
-    //cout << "6. Give me them all!" << endl;
-    cout << endl;
-
-    cout << "Please enter the number corresponding with the city you wish to research." << endl;
-    cout << "For example, please enter 1 for Gainesville." << endl;
-    
-    cin >> cityNum;
-    cityName = FindCity(cityNum);
-
-    while (!IsValidCity(cityName))
+    while(true)
     {
-        cout << "Sorry! We cannot find that city. Please enter a number from the menu above." << endl;
+        cout << "Weather Wizard is programmed to provide weather information for the following cities:" << endl;
+        cout << "1. Gainesville" << endl;
+        cout << "2. Jacksonville" << endl;
+        cout << "3. Miami" << endl;
+        cout << "4. Orlando" << endl;
+        cout << "5. Tampa" << endl; 
+        cout << endl;
+
+        cout << "Please enter the number corresponding with the city you wish to research. For example, please enter 1 for Gainesville." << endl;
+        cout << "If you wish to exit, enter 6." << endl;
+        
         cin >> cityNum;
+
+        //exit
+        if (cityNum == "6")
+            break;
+        
         cityName = FindCity(cityNum);
+
+        while (!IsValidCity(cityName))
+        {
+            cout << "Sorry! We cannot find that city. Please enter a number from the menu above." << endl;
+            cin >> cityNum;
+            cityName = FindCity(cityNum);
+        }
+
+        City newCity(cityName);
+        newCity.ReadAllFiles();
+
+        cout << endl << "Exellent choice! Now, enter your search start date, formatted YYYY-MM-DD." << endl;
+        cin >> startDate;
+
+        //chceck for input validity
+        while(!IsValidDate(newCity, startDate))
+        {
+            cout << "Sorry! We cannot find that date. Please enter another start date." << endl;
+            cin >> startDate;
+        }
+
+        cout << endl<< "Enter your search end date, formatted YYYY-MM-DD." << endl;
+        cin >> endDate;
+
+        //validate departure date input
+        while(!IsValidDate(newCity, endDate) || !IsValidRange(startDate, endDate))
+        {
+            if (!IsValidDate(newCity, endDate) )
+                cout << "Sorry! We cannot find that date. Please enter another end date." << endl;
+            else if (!IsValidRange(startDate, endDate))
+                cout << "Sorry! That date does not occur after your start date. Please enter another end date." << endl;
+
+            cin >> endDate;
+        }
+
+        //create new map object to store date range
+        map<string, Date*> newMap;
+        newCity.assembleMapBetweenDates(newMap, startDate, endDate);
+
+        cout << endl << endl << "Making magic happen..." << endl << endl;
+
+        //results
+        cout << endl << "For dates " << startDate << " through " << endDate << ":" << endl << endl;
+        
+        cout << "(Calculated with std::sort)" << endl;
+        cout << "Highest temperature, calculated in " << newCity.findHighestTemperature(newMap, "std_sort").second.count() << " microseconds: " << newCity.findHighestTemperature(newMap, "std_sort").first->air_temp << " degrees celsius" << endl;
+        cout << "Lowest temperature, calculated in " << newCity.findLowestTemperature(newMap, "std_sort").second.count() << " microseconds: " << newCity.findLowestTemperature(newMap, "std_sort").first->air_temp << " degrees celsius" << endl;
+        cout << "Fastest wind speed, calculated in " << newCity.findMaxWindSpeed(newMap, "std_sort").second.count() << " microseconds: " << newCity.findMaxWindSpeed(newMap, "std_sort").first->wind_speed << " meters per second" << endl;
+        cout << "Highest precipitation, calculated in " << newCity.findMaxPrecipitation(newMap, "std_sort").second.count() << " microseconds: " << newCity.findMaxPrecipitation(newMap, "std_sort").first->precipitation << " millimeters per hour" << endl;
+
+        //*******************************************************************************
+        //repeated with second sort
+        cout << endl << "(Calculated with merge sort)" << endl;
+        cout << "Highest temperature, calculated in " << newCity.findHighestTemperature(newMap, "merge_sort").second.count() << " microseconds: " << newCity.findHighestTemperature(newMap, "merge_sort").first->air_temp << " degrees celsius" << endl;
+        cout << "Lowest temperature, calculated in " << newCity.findLowestTemperature(newMap, "merge_sort").second.count() << " microseconds: " << newCity.findLowestTemperature(newMap, "merge_sort").first->air_temp << " degrees celsius" << endl;
+        cout << "Fastest wind speed, calculated in " << newCity.findMaxWindSpeed(newMap, "merge_sort").second.count() << " microseconds: " << newCity.findMaxWindSpeed(newMap, "merge_sort").first->wind_speed << " meters per second" << endl;
+        cout << "Highest precipitation, calculated in " << newCity.findMaxPrecipitation(newMap, "merge_sort").second.count() << " microseconds: " << newCity.findMaxPrecipitation(newMap, "merge_sort").first->precipitation << " millimeters per hour"<< endl;
+        cout << endl << endl;
     }
-
-    City newCity(cityName);
-    newCity.ReadAllFiles();
-
-    cout << endl << "Exellent choice! Now, enter your intended arrival date, formatted YYYY-MM-DD." << endl;
-    cin >> arrivalInput;
-
-    //chceck for input validity
-    while(!IsValidDate(newCity, arrivalInput))
-    {
-        cout << "Sorry! We cannot find that date. Please enter another arrival date." << endl;
-        cin >> arrivalInput;
-    }
-
-    cout << endl<< "Enter your intended departure date, formatted YYYY-MM-DD." << endl;
-    cin >> departureInput;
-
-    //validate departure date input
-    while(!IsValidDate(newCity, departureInput) || !IsValidRange(arrivalInput, departureInput))
-    {
-        if (!IsValidDate(newCity, departureInput) )
-            cout << "Sorry! We cannot find that date. Please enter another departure date." << endl;
-        else if (!IsValidRange(arrivalInput, departureInput))
-            cout << "Sorry! That date does not occur after your arrival date. Please enter another departure date." << endl;
-
-        cin >> departureInput;
-    }
-
-    //create new map object to store date range
-    map<string, Date*> newMap;
-    newCity.assembleMapBetweenDates(newMap, arrivalInput, departureInput);
-
-    cout << endl << endl << "Making magic happen..." << endl << endl;
-
-    //results
-    cout << endl << "For dates " << arrivalInput << " through " << departureInput << ":" << endl << endl;
-    
-    cout << "(Calculated with std::sort)" << endl;
-    cout << "Highest temperature, calculated in " << newCity.findHighestTemperature(newMap, "std_sort").second.count() << " microseconds: " << newCity.findHighestTemperature(newMap, "std_sort").first->air_temp << endl;
-    cout << "Lowest temperature, calculated in " << newCity.findLowestTemperature(newMap, "std_sort").second.count() << " microseconds: " << newCity.findLowestTemperature(newMap, "std_sort").first->air_temp << endl;
-    cout << "Fastest wind speed, calculated in " << newCity.findMaxWindSpeed(newMap, "std_sort").second.count() << " microseconds: " << newCity.findMaxWindSpeed(newMap, "std_sort").first->wind_speed << endl;
-    cout << "Highest precipitation, calculated in " << newCity.findMaxPrecipitation(newMap, "std_sort").second.count() << " microseconds: " << newCity.findMaxPrecipitation(newMap, "std_sort").first->precipitation << endl;
-
-    //*******************************************************************************
-    //repeated with second sort
-    cout << endl << "(Calculated with merge sort)" << endl;
-    cout << "Highest temperature, calculated in " << newCity.findHighestTemperature(newMap, "merge_sort").second.count() << " microseconds: " << newCity.findHighestTemperature(newMap, "merge_sort").first->air_temp << endl;
-    cout << "Lowest temperature, calculated in " << newCity.findLowestTemperature(newMap, "merge_sort").second.count() << " microseconds: " << newCity.findLowestTemperature(newMap, "merge_sort").first->air_temp << endl;
-    cout << "Fastest wind speed, calculated in " << newCity.findMaxWindSpeed(newMap, "merge_sort").second.count() << " microseconds: " << newCity.findMaxWindSpeed(newMap, "merge_sort").first->wind_speed << endl;
-    cout << "Highest precipitation, calculated in " << newCity.findMaxPrecipitation(newMap, "merge_sort").second.count() << " microseconds: " << newCity.findMaxPrecipitation(newMap, "merge_sort").first->precipitation << endl;
 
     return 0;
 }
@@ -131,28 +136,10 @@ bool IsValidDate(City city, string date)
     return true;
 }
 
-bool IsValidRange(string arrivalDate, string departureDate)
+bool IsValidRange(string startDate, string endDate)
 {
     //I think this will work, but need to test;
-    if (arrivalDate > departureDate)
+    if (startDate > endDate)
         return false;
     return true;
-}
-
-void LoadAllCities()
-{
-    City Gainesville("Gainesville");
-    Gainesville.ReadAllFiles();
-
-    City Jacksonville("Jacksonville");
-    Jacksonville.ReadAllFiles();
-
-    City Miami("Miami");
-    Miami.ReadAllFiles();
-
-    City Orlando("Orlando");
-    Orlando.ReadAllFiles();
-
-    City Tampa("Tampa");
-    Tampa.ReadAllFiles();
 }
